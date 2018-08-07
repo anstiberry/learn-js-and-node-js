ee34ab9be3172f9a51ea7c34e27a2ff05bb536cb
anavozhko <anastasiia.navozhko@gmail.com>
2018-08-07 10:45:09 +0300

    added mongoose

--- screencast/part2/chat/createDb.js
+++ screencast/part2/chat/createDb.js
@@ -1,63 +1,13 @@
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+var User = require('models/user').User;
 
-// Connection URL
-const url = 'mongodb://localhost:27017';
-
-// Database Name
-const dbName = 'chat';
-
-// Use connect method to connect to the server
-MongoClient.connect(url, function(err, client) {
-    assert.equal(null, err);
-    console.log("Connected successfully to server");
-
-    const db = client.db(dbName);
-
-    removeDocument(db, function() {
-        insertDocuments(db, function() {
-            findDocuments(db, function() {
-                client.close();
-            });
-        });
-    });
+var user = new User({
+    username: 'Tester2',
+    password: 'secret'
 });
 
-const insertDocuments = function(db, callback) {
-    // Get the documents collection
-    const collection = db.collection('documents');
-    // Insert some documents
-    collection.insertMany([
-        {a : 1}, {a : 2}, {a : 3}
-    ], function(err, result) {
-        assert.equal(err, null);
-        assert.equal(3, result.result.n);
-        assert.equal(3, result.ops.length);
-        console.log("Inserted 3 documents into the collection");
-        callback(result);
-    });
-};
-
-const findDocuments = function(db, callback) {
-    // Get the documents collection
-    const collection = db.collection('documents');
-    // Find some documents
-    collection.find({}).toArray(function(err, docs) {
-        assert.equal(err, null);
-        console.log("Found the following records");
-        console.log(docs);
-        callback(docs);
-    });
-};
-
-const removeDocument = function(db, callback) {
-    // Get the documents collection
-    const collection = db.collection('documents');
-    // Delete document where a is 3
-    collection.deleteMany({}, function(err, result) {
-        assert.equal(err, null);
-        // assert.equal(3, result.result.n);
-        console.log("Removed the document with the field a equal to 3");
-        callback(result);
-    });
-};
+user.save(function (err, user, affected) {
+    if (err) throw err;
+    User.findOne({username: 'Tester'}, function (err, tester) {
+        console.log(tester);
+    })
+});
