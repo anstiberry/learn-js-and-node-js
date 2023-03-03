a2c3bdae26461ca23ee51a7d3eeb596cd472332c
Anastasiia Riabushkina <anastasiia.riabushkina@gmail.com>
2023-03-04 01:19:26 +0200

    fix: remove image-server example

--- examples/image-server/config/dev.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-const dotenv = require('dotenv');
-dotenv.config();
-
-module.exports = {
-  host: '0.0.0.0',
-  port: 3009,
-  pathToImagesDir: process.env.PATH_TO_IMAGES_DIR || path.join(__dirname, '..', '/images/'),
-};
