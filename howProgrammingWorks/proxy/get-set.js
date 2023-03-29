6037767e2b96449f705b74b4b092c47d319c7b9e
Anastasiia Riabushkina <anastasiia.riabushkina@gmail.com>
2023-03-30 00:15:22 +0300

    feature: add proxy examples from hpw repository

---
+++ howProgrammingWorks/proxy/get-set.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const data = { name: 'Marcus Aurelius', city: 'Rome', born: 121 };
+
+const person = new Proxy(data, {
+  get(obj, key) {
+    console.log('get', key);
+    return obj[key];
+  },
+  set(obj, key, val) {
+    console.log('set', key, val);
+    obj[key] = val;
+    return true;
+  }
+});
+
+console.dir({ 'person.born': person.born });
+
+for (const key in person) {
+  console.dir({ key: person[key] });
+}
+
+person.name = 'Marcus';
